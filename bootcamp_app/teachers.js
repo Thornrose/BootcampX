4aa02dc1b309a7ce50deb87b701d4bd1428bd99d
Cheever Esler <ames.esler@gmail.com>
2022-11-16 22:38:42 -0400

    update to use parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,19 +7,31 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+
+
+
+
+const queryString = `
 SELECT teachers.name AS teacher, cohorts.name AS cohort
 FROM assistance_requests
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = cohort_id
 JOIN teachers ON teachers.id = teacher_id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teacher;
-`)
+`;
+
+const cohortName = process.argv[2];
+// store potentially malicios values in an array!!
+const values = [`%${cohortName}`];
+
+pool.query(queryString, values)
 .then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.cohort}: ${user.teacher}`);
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
   })
 })
 .catch(err => console.error('query error', err.stack));
+
+// also no output... mentor time
